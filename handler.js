f23fecf14569b7eb8e9f28ec084def6af5e35dd2
Shane Garrity <shanedgarrity@gmail.com>
2018-08-02 21:58:13 -0400

    remove comments

--- handler.js
+++ handler.js
@@ -1,22 +1,3 @@
-// serverless node template
-
-// 'use strict';
-
-// module.exports.hello = (event, context, callback) => {
-//   const response = {
-//     statusCode: 200,
-//     body: JSON.stringify({
-//       message: 'Go Serverless v1.0! Your function executed successfully!',
-//       input: event,
-//     }),
-//   };
-
-//   callback(null, response);
-
-//   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-//   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-// };
-
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports.hello = (event, context, callback) => {  
